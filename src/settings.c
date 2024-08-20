350991d0c41c3a5a9a84cae0b5d2fc9944eb0b34
Evan Bailey <embailey02@gmail.com>
2024-08-20 08:42:38 -0500

    Updated settings

--- src/settings.c
+++ src/settings.c
@@ -1,6 +1,6 @@
 #include <stdbool.h>
 
-// Repeat the declarations in settings.h without "extern"
-#define GIBI_SETTINGS_NOEXTERN
+// Repeat the declarations in settings.h without `extern`
+#define GIBI_SETTINGS_NO_EXTERN
 #include "settings.h"
 
--- src/settings.h
+++ src/settings.h
@@ -1,14 +1,25 @@
 // Global settings
+// In the future, this may be refactored into a non-global context struct.
 
 #ifndef GIBI_SETTINGS_H
 #define GIBI_SETTINGS_H
 
-// settings.c doesn't have to re-declare these settings without extern
-#ifdef GIBI_SETTINGS_NOEXTERN
+#include <stdlib.h>
+
+// settings.c doesn't have to re-declare these settings without extern but no ODR violations occur.
+#ifdef GIBI_SETTINGS_NO_EXTERN
 #define EXTERN
 #else
 #define EXTERN extern
 #endif
+
+// TODO add persistence
+
+// Theme settings
+
+// Miscellaneous display settings
+// Gap between tab stops. Must always be positive.
+EXTERN size_t tab_width = 8;
 
 #endif
 
